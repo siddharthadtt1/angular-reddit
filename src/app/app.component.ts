483d1fc2cfab3362200dbb708c562e148f5a0a11
Siddhartha Datta <siddharthadtt1@gmail.com>
2017-07-27 11:11:33 +0530

    Added multiple choices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	articles: Article[];
+	
+	constructor(){
+		this.articles = [ new Article("Angular 4", "http://angular.io", 3),
+						new Article("Java", "http://angular.io", 2),
+						new Article("Python", "http://angular.io", 1)
+		];
+	}
+	
 	addArticle(title: HTMLInputElement, link: HTMLInputElement){
 		console.log(`Adding title : ${title.value} with link : ${link.value}`);
 	}
